d1dd811dee06d61a5c7e05c3f79d6fe2a066cb8a
jsantis <san21153@uvg.edu.gt>
2023-08-10 23:42:28 -0600

    adelanto de shaders

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from gl import Renderer
+import shaders
+
+# El tama o del FrameBuffer
+width = 960
+height = 960
+
+# Se crea el renderizador
+rend = Renderer(width, height)
+
+# Le damos los shaders que se utilizar s
+rend.vertexShader = shaders.vertexShader
+rend.fragmentShader = shaders.fragmentShader
+
+# Cargamos los modelos que rederizaremos
+rend.glLoadModel(filename = "Dragon.obj",
+                 textureName = "Textures/modelD.bmp",
+                 translate = (width/4, height/4, 0),
+                 rotate = (45, 0, 0),
+                 scale = (10,10,10))
+
+
+# Se renderiza la escena
+rend.glRender()
+
+# Se crea el FrameBuffer con la escena renderizada
+rend.glFinish("toonshader.bmp")
